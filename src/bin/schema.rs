f66017992b5d79539a4c0ddff010cbffc69a2e6e
noodles <xiongty@outlook.com>
2023-02-18 17:46:22 +0800

    init

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use qg_injectvie::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
